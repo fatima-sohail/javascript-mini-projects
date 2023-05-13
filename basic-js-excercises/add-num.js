f025a6615dbde08405e947c7c53d541f8cb0b8df
unknown <fatimasohailhunzai@gmail.com>
2023-05-13 15:54:56 -0400

    added add-num.js file

---
+++ basic-js-excercises/add-num.js
@@ -0,0 +1,9 @@
+// Write a JavaScript function that takes two numbers as input and returns their sum.
+
+function addNum (x,y){
+    let sum = x + y;
+    return sum
+}
+console.log(addNum(5,6))
+console.log(addNum(3,6))
+console.log(addNum(5,4))
