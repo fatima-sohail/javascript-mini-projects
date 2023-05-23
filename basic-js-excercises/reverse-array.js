1a388020275d9338914359197449c039c65668ab
unknown <fatimasohailhunzai@gmail.com>
2023-05-22 21:33:10 -0400

    added reverse-array.js

---
+++ basic-js-excercises/reverse-array.js
@@ -0,0 +1,9 @@
+//Array Reversal. Write a function that takes an array as input and 
+//returns a new array with the elements in reverse order.
+
+let arr = [0, 1, 2]
+function reverseArray(item){
+    return item.reverse()
+}
+
+console.log(reverseArray(arr));
