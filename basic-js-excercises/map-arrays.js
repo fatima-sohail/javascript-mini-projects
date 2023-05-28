f5ac8fe1725d2cbfea4159297bfc0cf9c5bab312
unknown <fatimasohailhunzai@gmail.com>
2023-05-27 22:21:16 -0400

    added map-arrays.js file

---
+++ basic-js-excercises/map-arrays.js
@@ -0,0 +1,31 @@
+//map is used to transform an array into a new one with modified values e.g converting array of number into array of their squares or 
+//converting array of string into array of their lengths
+//Task 1:Array Mapping Write a function that takes an array of names as 
+//input and returns a new array with all the names capitalized.
+
+function capitalNames(array){
+    const mapNames = array.map((element)=> //no curly braces here!!
+        element.toUpperCase()
+    )
+    return mapNames
+}
+
+TestArr1 = ['a', 'b', 'c']
+console.log(capitalNames(TestArr1))
+
+console.log('-------------------------------')
+
+//Task2: (Multiplication) Write a function that takes an array of numbers as input 
+//and returns a new array with each number multiplied by 2.
+
+function arrMultipliedBy2(arr){
+    const mapNumber = arr.map((element) => 
+    element*2
+    )
+    return mapNumber;
+}
+
+testArr2 = [2,3,4];
+
+console.log(arrMultipliedBy2(testArr2));
+
