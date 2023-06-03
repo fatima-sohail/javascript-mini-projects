b2c1f0521df1ed6ed601c8dcf762ccb32e27b5fd
unknown <fatimasohailhunzai@gmail.com>
2023-06-03 16:07:14 -0400

    added nested-loops.pyramid-shape js file

---
+++ basic-js-excercises/nested-loops-pyramid-shape.js
@@ -0,0 +1,26 @@
+//Create a nested loop that prints a pattern of numbers in a pyramid shape, 
+//starting from 1 and increasing by 1 in each row.
+/*
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
+***********
+*/
+let square =  []
+for(let i = 1; i <= 10; i++){
+    let row = ''
+    for(let j=1; j<=10; j++){
+        row = row + '*'
+    }  
+    square.push(row) 
+}
+console.log(square)
+
+for(let k = 9; k>=1; k--){
+
+    for(let m = 1; m <=5; m++){
+        square[]
+    }
+}
