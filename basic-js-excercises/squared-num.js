0dc1fc085df4bb013602d68b12c944b47d2b9ff5
unknown <fatimasohailhunzai@gmail.com>
2023-06-13 17:28:49 -0400

    added squared-num.js

---
+++ basic-js-excercises/squared-num.js
@@ -0,0 +1,10 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//Square(n) Sum
+//For example, for [1, 2, 2] it should return 9
+
+function squareSum(numbers){
+    let squaredNums = numbers.map( (nun) => Math.pow(num,2));
+      return numbers
+    }
+    
+    console.log(squareSum([1,2,2]))
