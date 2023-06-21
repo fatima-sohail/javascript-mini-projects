a8e6160f0f6d0ac6a92f2e8a7b2a32d3f1591c7b
unknown <fatimasohailhunzai@gmail.com>
2023-06-21 16:24:19 -0400

    added string-length.js file

---
+++ basic-js-excercises/string-length.js
@@ -0,0 +1,13 @@
+//3. Write a JavaScript function that takes a string as input and returns the length of the string.
+function lengthOfStrng (input){
+   return input.length
+}
+
+console.log(lengthOfStrng("cat"))
+console.log(lengthOfStrng("dog"))
+console.log(lengthOfStrng("banana"))
+
+console.log("-----------------------------------")
+
+
+
