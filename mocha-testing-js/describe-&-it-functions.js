ed5a2b285b189dc2354a1f8de749d5974f513635
unknown <fatimasohailhunzai@gmail.com>
2023-07-08 20:32:09 -0400

    added code to the js file

---
+++ mocha-testing-js/describe-&-it-functions.js
@@ -0,0 +1,14 @@
+/*If you are testing a Math object with the method .min, you could use the following test code.*/
+
+//we group the tests in describe func
+//we define the tests in it func
+//both accept 2 parameters:
+//a discriptive strg
+//a callback func
+describe("Math", ()=> {
+    describe(".min", ()=> {
+        it("returns the argument with the lowest value", ()=>{
+
+        })
+    })
+})
