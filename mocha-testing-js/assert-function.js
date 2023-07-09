209932160c648d1119a29f6fd09acc4339634043
unknown <fatimasohailhunzai@gmail.com>
2023-07-09 18:59:47 -0400

    added code to the js file

---
+++ mocha-testing-js/assert-function.js
@@ -0,0 +1,10 @@
+// import assert here
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of its arguments', () => {
+    // Write assertion here
+    //this function allows you to compare values and throw errors as needed using one function call
+    assert.ok(3 + 4 === 7)
+  });
+});
