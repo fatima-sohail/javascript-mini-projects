fb150aea77343b63cd7d5ae0cc6828c773ad5af2
unknown <fatimasohailhunzai@gmail.com>
2023-07-16 09:15:39 -0400

    added code to the js file

---
+++ mocha-testing-js/setup-exercise-verify-steps-in-test.js
@@ -0,0 +1,28 @@
+//naive approach vs 3 step approach
+//setup, exercise and varify steps in test functions
+
+const assert = require('assert');
+
+// Naive approach
+describe('.pop', () => {
+  it('returns the last element in the array [naive]', () => {
+    assert.ok(['padawan', 'knight'].pop() === 'knight'); 
+  });
+});
+//run npm test in terminal, it will pass
+
+// 3 phase approach
+describe('.pop', () => {
+  it('returns the last element in the array [3phase]', () => {
+    // Setup
+    const knightString = 'knight';
+    const jediPath = ['padawan', knightString];
+
+    // Exercise
+    const popped = jediPath.pop();
+
+    // Verify
+    assert.ok(popped === knightString);
+  });
+});
+//run npm test in terminal, it will pass
