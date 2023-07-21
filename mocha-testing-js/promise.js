09b9a1337f7065b6d89965c744c5cac9662eb7d7
unknown <fatimasohailhunzai@gmail.com>
2023-07-21 09:10:02 -0400

    added js file

---
+++ mocha-testing-js/promise.js
@@ -0,0 +1,12 @@
+ 
+  
+
+  console.log("This is the first line of code in app.js.");
+  // Keep the line above as the first line of code
+  // Write your code here:
+  const usingSTO = ()=> {
+    console.log('we are alive and we will thrive')
+  }
+  
+  setTimeout(usingSTO, 2000)
+  
