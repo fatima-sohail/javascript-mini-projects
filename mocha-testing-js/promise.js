7ebdef80be69727a32d4c162fc32d02c12cf49f9
unknown <fatimasohailhunzai@gmail.com>
2023-07-25 11:42:57 -0400

    added js file

--- mocha-testing-js/promise.js
+++ mocha-testing-js/promise.js
@@ -9,4 +9,40 @@
   }
   
   setTimeout(usingSTO, 2000)
+
+  // Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
+
+//prints: This is the first line of code in app.js.
+        //This is the last line of code in app.js.
+        //we are alive and we will thrive
+
   
+//****************************************************************************************
+
+  let prom = new Promise((resolve, reject) => {
+    let num = Math.random();
+    if (num < .5 ){
+      resolve('Yay!');
+    } else {
+      reject('Ohhh noooo!');
+    }
+  });
+   
+  const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+  };
+   
+  const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+  };
+   
+  prom.then(handleSuccess, handleFailure);
+  
+  //prints both statments randomly: ohh noo
+  //prints: Yay! 
+  
+  //**************************************************************** */
+
+  
+
